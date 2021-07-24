83678538ba8b754e771a3e92d99b301f7cd78370
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-07-24 17:50:54 -0400

    add strong params for climbs and fix CORS errors

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -9,4 +9,12 @@ class ClimbsController < ApplicationController
         climb = Climb.find_by(id: params[:id])
         render json: climb
     end
+
+    def create
+
+    private
+
+    def climb_params
+        params.require(:climb).permit(:climb_type, :location, :color, :grade, :notes)
+    end 
 end
