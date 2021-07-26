beb1cb1dcebe4643b188f2feae59fa5051de6572
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-07-26 18:39:13 -0400

    make a fetch request to test api and js setup

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("test");
+
+const climbsContainer = document.querySelector(".climbs-container");
+
+console.log(climbsContainer);
+
+fetch("http://127.0.0.1:3000/climbs").then( function(res) {
+    console.log(res.json())
+});
